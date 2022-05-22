2ec500912ea4d268006ee7afcf694ecea2863739
Igor Kim <igor.skh@gmail.com>
2022-05-22 12:10:16 +0200

    add more tea data to sync repository

--- app/lib/core/repositories/sync_tea_repository.dart
+++ app/lib/core/repositories/sync_tea_repository.dart
@@ -23,7 +23,11 @@ class SyncTeaRepository implements AbstractTeaRepository {
         origin: "Japan",
         categoryID: 'c2',
         category: teaCategories[1],
-        steepingTime: 20,
+        steepingTime: 120,
+        steepingAmount: "3 tbsp",
+        steepingCount: 3,
+        steepingTemperature: 70,
+        amountOfWater: 100,
         description:
             "An exquisite fully shaded tea with an unusual green colour and a sweet-spicy aroma that lingers for a long time.\n\nFully shaded teas from organic cultivation are particularly rare, as the tea plants are also particularly prone to parasites in the strong shade and special diligence and experience are required for cultivation. However, the effort is rewarded with a very special, multifaceted density of aromas.",
         imageURL:
@@ -35,7 +39,11 @@ class SyncTeaRepository implements AbstractTeaRepository {
         origin: "Japan",
         categoryID: 'c1',
         category: teaCategories[0],
-        steepingTime: 30,
+        steepingTime: 180,
+        steepingAmount: "3 tbsp",
+        steepingCount: 3,
+        steepingTemperature: 90,
+        amountOfWater: 120,
         description:
             "Japanese wakocha (和紅茶) is one of the best varieties of black tea in the world, alongside those from Darjeeling, Sri Lanka and China. Its elegant yet subtle flavour which carries delicate and aromatic floral notes, as well as its strong and bright cup colour, puts Japanese black tea in a class of its own. Traditional farmers in Japan are starting to dedicate themselves to the almost forgotten art of black tea production in ever increasing numbers. We source our wakocha from award-winning farmers producing wakocha in Japan's most renowned black tea terroirs.",
         imageURL:
